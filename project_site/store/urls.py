74548c1df4e8af2f946e27dde9e9950152c31f93
mdonkorka <mdonkorka@gmail.com>
2020-12-06 14:41:30 +0000

    Build off point - set up base for different urls and main template inheritance.

--- project_site/store/urls.py
+++ project_site/store/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 
 from . import views
 urlpatterns = [
-    path('', views.index, name='homepage'),
-    path('about/', views.aboutPage, name='about'),
+    path('', views.homepage, name="homepage"),
+    path('store/', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('about/', views.about, name="about" ),
 ]
--- project_site/store/views.py
+++ project_site/store/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
-def index(request):
+def homepage(request):
     
     context = {}
-    return render(request, 'store/index.html', context)
+    return render(request, 'store/homepage.html', context)
 
 # Create your views here.
-def aboutPage(request):
+def store(request):
+    
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def about(request):
     
     context = {}
     return render(request, 'store/about.html', context)
+
+def cart(request):
+    
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    
+    context = {}
+    return render(request, 'store/checkout.html', context)

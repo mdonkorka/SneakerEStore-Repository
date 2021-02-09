ce158f4d7dd09adf066dc834eaa496ef49c4e7e5
Michael D <mdonkorka@gmail.com>
2021-02-09 19:37:36 +0000

    Data Structure Complete

--- project_site/store/views.py
+++ project_site/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+
+from .models import *  
+
+
+
+
 def homepage(request):
     
     context = {}
@@ -8,9 +14,9 @@ def homepage(request):
 
 # Create your views here.
 def store(request):
-    
-    context = {}
-    return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def about(request):
     
@@ -19,10 +25,38 @@ def about(request):
 
 def cart(request):
     
-    context = {}
+
+    if request.user.is_authenticated:
+	    customer = request.user.customer
+	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	    items = order.orderitem_set.all()
+    else:
+	    #Create empty cart for now for non-logged in user
+	    items = []
+	    order = {'get_cart_total':0, 'get_cart_items':0}
+
+
+    context = {'items' :items, 'order' :order}
     return render(request, 'store/cart.html', context)
 
+    
+
 def checkout(request):
+
+
+    if request.user.is_authenticated:
+	    customer = request.user.customer
+	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	    items = order.orderitem_set.all()
+    else:
+	    #Create empty cart for now for non-logged in user
+	    items = []
+	    order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items' :items, 'order' :order}
+    return render(request, 'store/checkout.html', context)
+
+
     
     context = {}
     return render(request, 'store/checkout.html', context)
